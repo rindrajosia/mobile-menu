904c9bfbe137263febfe423f9d7f7d2246e13b8a
rindra josia <rrindrajosia@gmail.com>
2021-07-24 15:01:20 +0300

    Add function myFunction

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".icon");
+const active = document.querySelector(".active");
+const close = document.querySelector(".close");
+const navLink = document.querySelectorAll(".mobile-link");
+
+hamburger.addEventListener("click", myFunction);
+navLink.forEach(n => n.addEventListener("click", myFunction));
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    close.style.display = "none";
+    active.style.display = "block";
+  } else {
+    x.style.display = "block";
+    close.style.display = "block";
+    active.style.display = "none";
+  }
+}
