4cca13bc8e7fc7aeecd2b4bae65a6160fa017ef1
rindra josia <rrindrajosia@gmail.com>
2021-07-26 07:12:22 +0300

    Change variable name js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ const close = document.querySelector('.close');
 const navLink = document.querySelectorAll('.mobile-link');
 
 function myFunction() {
-  const x = document.getElementById('myLinks');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
+  const link = document.getElementById('myLinks');
+  if (link.style.display === 'block') {
+    link.style.display = 'none';
     close.style.display = 'none';
     active.style.display = 'block';
   } else {
-    x.style.display = 'block';
+    link.style.display = 'block';
     close.style.display = 'block';
     active.style.display = 'none';
   }
